78d25c0cb72019f7fbd3643362bd7fd8974f77ac
santetem <santiago.diazet32@gmail.com>
2025-06-30 12:23:19 -0300

    Add files via upload

---
+++ Ejercicio 6.py
@@ -0,0 +1,17 @@
+matriz = [
+    [1, 2, 8],
+    [3, 4, 5],
+    [9, 7, 6]
+]
+
+numero_max = -1 
+fila = 0
+
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])): # <-- ¡Aquí está el cambio!
+        if matriz[i][j] > numero_max:
+            numero_max = matriz[i][j]
+            fial = j
+
+print(f"El número máximo en la matriz es: {numero_max}")
+print(f"Se encuentra en la columna: {fila}")
