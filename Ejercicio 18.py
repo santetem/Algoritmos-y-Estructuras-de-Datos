6366c769fe379f88d36945c494f738507e7352de
santetem <santiago.diazet32@gmail.com>
2025-07-11 09:44:05 -0300

    Add files via upload

---
+++ Ejercicio 18.py
@@ -0,0 +1,28 @@
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print("Matriz original:")
+for fila in matriz:
+    print(fila)
+
+def consigna(m):
+    filas = len(m)
+    columnas = len(m[0])
+
+    transpuesta = []
+    for j in range(columnas):
+        nuevaFila = []
+        for i in range(filas):
+            nuevaFila.append(m[i][j])
+        transpuesta.append(nuevaFila)
+    
+    return transpuesta
+
+transpuesta = consigna(matriz)
+
+print("\nMatriz transpuesta:")
+for fila in transpuesta:
+    print(fila)
